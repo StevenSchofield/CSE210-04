04c0490f86d56179fc3b1eb4a0e829c3909a7d95
StevenSchofield <sch21093@byui.edu>
2022-02-18 12:55:17 -0800

    Added score requirements

--- __main__.py
+++ __main__.py
@@ -21,7 +21,7 @@ CELL_SIZE = 15
 FONT_SIZE = 15
 COLS = 60
 ROWS = 40
-CAPTION = "Robot Finds Kitten"
+CAPTION = "Greed"
 DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
@@ -31,14 +31,14 @@ def main():
     
     # create the cast
     cast = Cast()
-    
-    # create the banner
-    banner = Actor()
-    banner.set_text("")
-    banner.set_font_size(FONT_SIZE)
-    banner.set_color(WHITE)
-    banner.set_position(Coordinate(CELL_SIZE, 0))
-    cast.add_actor("banners", banner)
+
+    # create the score banner
+    score = Actor()
+    score.set_text("Score: 0")
+    score.set_font_size(FONT_SIZE)
+    score.set_color(WHITE)
+    score.set_position(Coordinate(CELL_SIZE, 0))
+    cast.add_actor("banners", score)
     
     # create the robot
     x = int(MAX_X / 2)

154e4dd82ab6a4a2cb0d2cd21a253255c4a43e33
StevenSchofield <sch21093@byui.edu>
2022-02-22 09:43:27 -0800

    Removed two lines of code
    They were not needed for this assignments

--- __main__.py
+++ __main__.py
@@ -2,7 +2,6 @@ import os
 import random
 
 from game.casting.actor import Actor
-from game.casting.artifact import Artifact
 from game.casting.cast import Cast
 
 from game.directing.director import Director
@@ -23,7 +22,6 @@ FONT_SIZE = 15
 COLS = 60
 ROWS = 40
 CAPTION = "Greed"
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
 SPAWN_RATE = 100

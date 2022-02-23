3878424b3f8d03143cc95a322f3577f0ce267fd5
Engelito <anggon3.ag@gmail.com>
2022-02-23 09:48:35 -0700

    deleted underscore

--- game/casting/gems.py
+++ game/casting/gems.py
@@ -3,6 +3,6 @@ class Gems(FallingObject):
     def __init__(self, score=5):
         super().__init__()
         self._score = score
-        self.set_text(['*']);
+        self.set_text(['*'])
     def getScore(self):
         return self._score
--- game/casting/rocks.py
+++ game/casting/rocks.py
@@ -3,8 +3,8 @@ from game.casting.fallingObject import FallingObject
 class Rocks(FallingObject):
     def __init__(self):
         super().__init__()
-        _hit = FALSE;
-        self.set_text(['O']);
+        self._hit = FALSE;
+        self.set_text(['O'])
     def getHit(self):
         return self._hit
         

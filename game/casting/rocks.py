676b7cbb7ca5db277f92be3954c46ceac4d0f99f
Nathan Wood <nw14943@byui.edu>
2022-02-25 11:37:11 -0700

    Modified gem and rock
    Fixed the set_text call

--- game/casting/rocks.py
+++ game/casting/rocks.py
@@ -4,7 +4,7 @@ class Rocks(FallingObject):
     def __init__(self):
         super().__init__()
         self._hit = FALSE
-        self.set_text(['O'])
+        self.set_text('O')
     def getHit(self):
         return self._hit
         

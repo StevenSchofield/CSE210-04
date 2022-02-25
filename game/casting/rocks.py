624d87daa8e37f749f587590111e81f5f9bfcbe2
Engelito <anggon3.ag@gmail.com>
2022-02-25 12:19:28 -0700

    comments for rocks and falling object.

--- game/casting/rocks.py
+++ game/casting/rocks.py
@@ -1,5 +1,8 @@
 from pickle import FALSE
 from game.casting.fallingObject import FallingObject
+""""Rocks will inherit from falling object and 
+    will have a hit function that will be activated
+    when there is a collision."""
 class Rocks(FallingObject):
     def __init__(self):
         super().__init__()
